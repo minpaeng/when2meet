7a7e7267080aee6072bf473c28a8a68dbbd1d3ce
minpaeng <22alswjd@naver.com>
2022-04-06 20:45:01 +0900

    텔레그램 봇 1차 완료: 리팩토링 필요

--- DB/queries.py
+++ DB/queries.py
@@ -7,20 +7,21 @@ def select_all_chat_group() -> tuple:
     sql = "SELECT * FROM `chat_group`;"
     cursor.execute(sql)
     result = cursor.fetchall()
-    print(result)
     return result
 
 
-def insert_to_chat_group(query):
-    sql = query
+def select_message_by_grouop_id(group_id) -> tuple:
+    sql = "SELECT user_id, context FROM message WHERE group_id=" + str(group_id) + ";"
     cursor.execute(sql)
-    db.commit()
+    result = cursor.fetchall()
+    return result
 
 
-def update_chat_group(query):
-    sql = query  # UPDATE 테이블명 SET 컬럼명=컬럼값 WHERE절
-    cursor.execute(sql)
+def insert_to_chat_group(group_id, is_start):
+    sql = "INSERT INTO chat_group(group_id, is_start) VALUES(" + str(group_id) + ", " + str(is_start) + ");"
+    res = cursor.execute(sql)
     db.commit()
+    return res
 
 
 def delete_from_chat_group(group_id):
@@ -31,9 +32,32 @@ def delete_from_chat_group(group_id):
 
 def find_group_id_from_chat_group(group_id) -> bool:
     sql = "SELECT group_id FROM chat_group where group_id=" + str(group_id) + ';'
+    cursor.execute(sql)
     result = cursor.fetchall()
-    print(result)
     if len(result) != 1:
         return False
     else:
         return True
+
+
+def find_is_start_from_chat_group(group_id) -> bool:
+    sql = "SELECT is_start FROM chat_group where group_id=" + str(group_id) + ';'
+    cursor.execute(sql)
+    result = cursor.fetchall()
+    if result[0]['is_start'] == 1:
+        return True
+    else:
+        return False
+
+
+def insert_to_message(group_id, user_id, msg):
+    sql = "INSERT INTO message(group_id, user_id, context) VALUES (" \
+          + str(group_id) + ", " + str(user_id) + ", \"" + msg + "\");"
+    cursor.execute(sql)
+    db.commit()
+
+
+def set_is_start_1(group_id):
+    sql = "UPDATE chat_group SET is_start=1 WHERE group_id=" + str(group_id) + ";"
+    cursor.execute(sql)
+    db.commit()
--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from telegram import Update, Bot
 from telegram.ext import Updater, CallbackContext
-from telegram.ext import CommandHandler
+from telegram.ext import CommandHandler, MessageHandler, Filters
 from DB import queries
 
 
@@ -10,39 +10,113 @@ def read_token(file_path):
     return s
 
 
+# start 명령어
+# 1) chat - id(그룹일 때 음수일 수 있음) 조회
+# 2) chat_group 테이블에 그룹 아이디가 존재하는지 확인
+# 3) 존재한다면 is_start가 false인지 확인 후 true로 바꿔줌
+# 4) 존재하지 않는다면 Group 테이블에 그룹 추가(isStart는 true로 설정하여 추가)
+# 5) 예외처리: is_start가 true 상태라면 start 명령어를 중복호출한 것이므로 end명령어를 입력할 차례라고 안내
+# 6) 예외처리: chat_type이 private일 때 어떻게 처리할것인가? 1) 혼자서 대화 진행 가능하도록 2) 둘 이상 단체 채팅에서 사용하라고 경고
 def start(update: Update, context: CallbackContext) -> None:
-    # update.message.reply_text(f'Hello {update.effective_user.first_name}')
-    text = update.message.text
+    # 1) chat - id(그룹일 때 음수일 수 있음) 조회
+    msg = update.message
+    chat_type, group_id, _, _ = chat_info(msg)
+
+    # 6) 예외처리: chat_type이 private일 때 어떻게 처리할것인가? 1) 혼자서 대화 진행 가능하도록 2) 둘 이상 단체 채팅에서 사용하라고 경고
+    if chat_type == "private":
+        update.message.reply_text("그룹 채팅방에서 서비스를 이용해주세요.")
+        return
+
+    # 2) chat_group 테이블에 그룹 아이디가 존재하는지 확인
+    # 3) 존재한다면 is_start가 false인지 확인 후 true로 바꿔줌
+    if queries.find_group_id_from_chat_group(group_id):
+        print("group_id 존재: " + str(group_id))
+        # 5) 예외처리: is_start가 true 상태라면 start 명령어를 중복호출한 것이므로 end명령어를 입력할 차례라고 안내
+        if queries.find_is_start_from_chat_group(group_id):
+            update.message.reply_text("start 명령어가 실행중입니다. 대화를 진행해주세요.")
+            return
+        else:
+            queries.set_is_start_1(group_id)
+    else:
+        res = queries.insert_to_chat_group(group_id, 1)
+        print("group_id 삽입 완료: {}".format(res))
+
     update.message.reply_text("대화를 시작하세요.\n\"done\"을 입력하면 대화를 마치고, \n\
-내용을 분석하여 다함께 만날 수 있는 날을 요약해 줍니다.")
-    # if !queries.find_group_id_from_chat_group():
-        # queries.insert_to_chat_group("")
+    내용을 분석하여 다함께 만날 수 있는 날을 요약해 줍니다.")
 
 
+# end 명령어
+# 1) chat - id(그룹일 때 음수일 수 있음) 조회
+# 2) chat_group 테이블에 그룹 아이디가 존재하는지 확인
+# 3) 존재한다면 is_start가 true인지 확인: false라면 start 명령어를 먼저 호출해야 한다고 안내
+#                        (start 명령어를 통해 대화 시작 후 end 명령어를 입력하면 약속 시간을 알려줍니다! 하는 내용의 안내)
+# 4) 해당 group_id의 메세지를 모두 조회하여 user_id와 함께 모델에 전송 후 chat_group에서 삭제
+# 5) 예외처리: is_start가 false라면 start 명령어를 먼저 호출해야 한다고 안내
+# 6) 예외처리: chat_type이 private일 때 어떻게 처리할것인가? 1) 혼자서 대화 진행 가능하도록 2) 둘 이상 단체 채팅에서 사용하라고 경고
 def done(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text("다함께 만날 수 있는 시간은 아래와 같습니다.\n\n\
-1순위\n2022-03-31-00:00~2022-03-31-24:00\n\n\
-2순위\n2022-03-29-00:00~2022-03-29-24:00")
+    # 1) chat - id(그룹일 때 음수일 수 있음) 조회
+    msg = update.message
+    chat_type, group_id, _, _ = chat_info(msg)
+
+    # 6) 예외처리: chat_type이 private일 때 어떻게 처리할것인가? 1) 혼자서 대화 진행 가능하도록 2) 둘 이상 단체 채팅에서 사용하라고 경고
+    if chat_type == "private":
+        update.message.reply_text("그룹 채팅방에서 서비스를 이용해주세요.",
+                                  reply_to_message_id=msg.message_id)
+        return
+
+    # 2) chat_group 테이블에 그룹 아이디가 존재하는지 확인
+    if not queries.find_group_id_from_chat_group(group_id):
+        print("{}번 group_id 없음: ".format(str(group_id)))
+        update.message.reply_text("start 명령어를 먼저 호출한 후 대화를 시작해야 합니다.",
+                                  reply_to_message_id=msg.message_id)
+        return
+
+    # 3) 존재한다면 is_start가 true인지 확인: false라면 start 명령어를 먼저 호출해야 한다고 안내
+    if not queries.find_is_start_from_chat_group(group_id):
+        update.message.reply_text("start 명령어를 먼저 호출한 후 대화를 시작해야 합니다.",
+                                  reply_to_message_id=msg.message_id)
+        return
+    else:
+        # 그룹 아이디에 해당하는 대화를 DB에서 가져옴
+        res = queries.select_message_by_grouop_id(group_id)
+        print(res)
+
+        # 모델에 대화 넘기고 결과 받아오기
+
+        # chat_group과 message 테이블에서 해당 그룹 아이디와 관련된 값 모두 삭제
+        queries.delete_from_chat_group(group_id)
+
+        update.message.reply_text(reply_to_message_id=msg.message_id,
+                                  text="다함께 만날 수 있는 시간은 아래와 같습니다.\n\n\
+                                    1순위\n2022-03-31-00:00~2022-03-31-24:00\n\n\
+                                    2순위\n2022-03-29-00:00~2022-03-29-24:00")
+
+
+# start 명령어 호출 후 실행되는 메소드
+# 1) 그룹 아이디, 유저 아이디, 메세지 내용을 받아서 DB에 저장
+def receive_msg(update: Update, context: CallbackContext) -> None:
+    msg = update.message
+    _, group_id, user_id, text = chat_info(msg)
+    if not queries.find_group_id_from_chat_group(group_id):
+        return
+    queries.insert_to_message(group_id=group_id, user_id=user_id, msg= text)
+
+
+def chat_info(message):
+    chat_type = message.chat.type
+    group_id = message.chat.id
+    user_id = message.from_user.id
+    chat = message.text
+    return chat_type, group_id, user_id, chat
 
 
 token = read_token("token.txt")
-# bot = Bot(token=token)
 updater = Updater(token)
-commands = updater.bot.commands
-print(commands)
+
 
 updater.dispatcher.add_handler(CommandHandler('start', start))
 updater.dispatcher.add_handler(CommandHandler('done', done))
+updater.dispatcher.add_handler(MessageHandler(Filters.text, receive_msg))
 updater.start_polling()
 updater.idle()
 
-# chat_id = bot.getUpdates()[-1].message.chat.id
-# chat_text = bot.getUpdates()[-1].message.text
-# bot.sendMessage(chat_id=chat_id, text="test")
-# print(chat_id)
-# print(chat_text)
-
-
-# @bot.message_handler(func=lambda message: True)
-# def echo_all(message):
-#     bot.reply_to(message, message.text)
